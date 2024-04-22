9c0d2ad7fad3b414db007305f47792f025ffbf4d
bradsimpson213 <bradsimpson@icloud.com>
2024-04-22 17:49:32 -0400

    updated for rest of lecture

--- Mod-4/W10/D1/Patchstagram/server/app.js
+++ Mod-4/W10/D1/Patchstagram/server/app.js
@@ -1,15 +1,38 @@
 const express = require("express")
+const { someData, users } = require("./data")
 
 
 const app = express()
+app.use(express.json())
 
 
-
-app.get("/posts/all", (req, res) => {
-    res.send("Hey we hit this route!  yay!")
+app.get(["/posts/all", "/feed"], (req, res) => {
+    // res.send("Hey we hit this route!  yay!")
+    res.json(someData)
 })
 
 
+app.get("/posts/:id", (req, res) => {
+
+    console.log("REQ PARAMS",req.params.id)
+    const post = someData.find( post => (post.id === parseInt(req.params.id)))
+    console.log(post)
+    res.json(post)
+})
+
+
+app.post("/posts/new", (req, res) => {
+    console.log("WE HIT THIS ROUTE")
+    console.log(req.body)
+    res.send("This is the post route")
+})
+
+
+app.get("/users/search", (req, res) => {
+    console.log(req.query.name)
+    const selectedUser = users.find( user => user.name.includes(req.query.name))
+    res.json(selectedUser)
+})
 
 const port = 5000
 
