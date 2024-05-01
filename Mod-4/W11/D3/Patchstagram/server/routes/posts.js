16e0b716a7448298fda1a651a95b33dfd0b1b283
bradsimpson213 <bradsimpson@icloud.com>
2024-05-01 16:43:06 -0400

    lecture code and SP through create

--- Mod-4/W11/D3/Patchstagram/server/routes/posts.js
+++ Mod-4/W11/D3/Patchstagram/server/routes/posts.js
@@ -57,4 +57,33 @@ router.post("/new", async(req, res) => {
     res.json(resPost)
 })
 
+
+router.put("/update/:id", async (req, res) => {
+    const updatePost = await Post.findByPk(req.params.id)
+    console.log(updatePost)
+    const { title, image, author } = req.body
+
+    if (title !== undefined) updatePost.title = title;
+    if (author !== undefined) updatePost.author = author;
+    if (image !== undefined) updatePost.image = image;
+
+    await updatePost.save()
+
+    res.json({
+        message: `Successfully updated a post with id ${req.params.id}`,
+        data: updatePost
+    })
+})
+
+router.delete("/delete/:id", async (req, res) => {
+    const deletePost = await Post.findByPk(req.params.id)
+    console.log(deletePost)
+    await deletePost.destroy()
+
+    res.json({
+        message: `Successfully deleted post with id ${req.params.id}`
+    })
+
+})
+
 module.exports = router;
