992730251bbec16d73e02969741e484ae84ccd0b
bradsimpson213 <bradsimpson@icloud.com>
2024-05-09 10:15:14 -0400

    updated patchstagram for w12d4lecture

---
+++ Mod-4/W12/D4/Patchstagram/server/db/models/user.js
@@ -0,0 +1,70 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+
+    static associate(models) {
+      User.hasMany(
+        models.Post,
+        { foreignKey: "author"}
+      )
+
+      User.belongsToMany(
+        models.Post, {
+          foreignKey: "userId",
+          through: models.Like,
+          otherKey: "postId"
+        }
+      )
+    }
+  }
+  User.init({
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    },
+    profile: DataTypes.STRING(255),
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: {
+          args: [0],
+          msg: "Cats need a 'pawsitive' age"
+        },
+        max: 38,
+      },
+    },
+    breed: DataTypes.STRING(50),
+    favToy: {
+      type: DataTypes.STRING(50),
+      defaultValue: "mouse"
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
