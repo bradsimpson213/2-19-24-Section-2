f3fdc6bd6c872937bc891b584d1c3547a3e20815
bradsimpson213 <bradsimpson@icloud.com>
2024-05-28 16:42:47 -0400

    updated for slides and lecture code

--- Mod-5/W15/D1/react-dice/src/components/Die.jsx
+++ Mod-5/W15/D1/react-dice/src/components/Die.jsx
@@ -1,18 +1,28 @@
-import { Component } from 'react'
+// import { Component } from 'react'
 import './Die.css'
 
 // TODO: Refactor this component to be a functional component
 
-class Die extends Component {
-    render(){
-        return (
-            <div>
-                <i className={`Die fas fa-dice-${ this.props.face } 
-                    ${ this.props.shake ? 'shake-dice' : '' }`} />
-            </div>
-        );
-    }
+// class Die extends Component {
+//     render(){
+//         return (
+//             <div>
+//                 <i className={`Die fas fa-dice-${ this.props.face } 
+//                     ${ this.props.shake ? 'shake-dice' : '' }`} />
+//             </div>
+//         );
+//     }
+// }
+
+function Die ({face, shake}) {
+    return (
+        <div>
+            <i className={`Die fas fa-dice-${ face } 
+                ${ shake ? 'shake-dice' : '' }`} />
+        </div>
+    );
 }
+
 
 
 export default Die;
