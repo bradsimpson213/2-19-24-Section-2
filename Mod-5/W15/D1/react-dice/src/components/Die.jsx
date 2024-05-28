0e7c1ddf609be474eac4991cb557deb9666553f3
bradsimpson213 <bradsimpson@icloud.com>
2024-05-28 13:32:15 -0400

    updated for w15d1

---
+++ Mod-5/W15/D1/react-dice/src/components/Die.jsx
@@ -0,0 +1,19 @@
+import { Component } from 'react'
+import './Die.css'
+
+// TODO: Refactor this component to be a functional component
+
+class Die extends Component {
+    render(){
+        return (
+            <div>
+                <i className={`Die fas fa-dice-${ this.props.face } 
+                    ${ this.props.shake ? 'shake-dice' : '' }`} />
+            </div>
+        );
+    }
+}
+
+
+export default Die;
+
